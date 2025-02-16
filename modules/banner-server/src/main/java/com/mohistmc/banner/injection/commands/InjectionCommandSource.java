f79f8025a433190a188bc050f53237ccc33a8bee
Mgazul <mgazul@163.com>
2025-02-16 14:50:42 +0800

    Fix list command

--- modules/banner-server/src/main/java/com/mohistmc/banner/injection/commands/InjectionCommandSource.java
+++ modules/banner-server/src/main/java/com/mohistmc/banner/injection/commands/InjectionCommandSource.java
@@ -5,6 +5,6 @@ import net.minecraft.commands.CommandSourceStack;
 public interface InjectionCommandSource {
 
     default org.bukkit.command.CommandSender banner$getBukkitSender(CommandSourceStack wrapper) {
-        throw new IllegalStateException("Not implemented");
+        return null;
     }
 }
