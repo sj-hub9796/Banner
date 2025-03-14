fe6c8ab20bc669c550b7e7ef72eba1a0f4a29652
Mgazul <mgazul@163.com>
2025-03-14 22:09:28 +0800

    Fixed #380

--- modules/banner-server/src/main/java/com/mohistmc/banner/mixin/world/level/MixinExplosion.java
+++ modules/banner-server/src/main/java/com/mohistmc/banner/mixin/world/level/MixinExplosion.java
@@ -11,8 +11,10 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Explosion;
+import net.minecraft.world.level.ExplosionDamageCalculator;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.TntBlock;
@@ -27,6 +29,7 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -37,22 +40,24 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public abstract class MixinExplosion implements InjectionExplosion {
 
     @Shadow @Final public Entity source;
-    public boolean wasCanceled = false; // CraftBukkit - add field
-    public float yield;
     // @formatter:off
     @Shadow @Final private Level level;
     @Shadow @Final private Explosion.BlockInteraction blockInteraction;
+    @Shadow @Final private ExplosionDamageCalculator damageCalculator;
     @Shadow @Mutable @Final private float radius;
     @Shadow @Final private double x;
     @Shadow @Final private double y;
     @Shadow @Final private double z;
+    // @formatter:on
+
+    @Unique public boolean wasCanceled = false; // CraftBukkit - add field
+    @Unique public float yield;
 
     @Inject(method = "addOrAppendStack", cancellable = true, at = @At("HEAD"))
     private static void banner$fix(List<Pair<ItemStack, BlockPos>> p_311090_, ItemStack stack, BlockPos p_309821_, CallbackInfo ci) {
         if (stack.isEmpty()) ci.cancel();
     }
 
-    // @formatter:on
     @Inject(method = "<init>(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/entity/Entity;DDDFZLnet/minecraft/world/level/Explosion$BlockInteraction;)V",
             at = @At("RETURN"))
     public void banner$adjustSize(Level worldIn, Entity exploderIn, double xIn, double yIn, double zIn, float sizeIn, boolean causesFireIn, Explosion.BlockInteraction modeIn, CallbackInfo ci) {
@@ -63,6 +68,15 @@ public abstract class MixinExplosion implements InjectionExplosion {
     @Override
     public float bridge$getYield() {
         return yield;
+    }
+
+    @Inject(method = "explode", at = @At(value = "HEAD"), cancellable = true)
+    public void banner$explode(CallbackInfo ci) {
+        // CraftBukkit start
+        if (this.radius < 0.1F) {
+            ci.cancel();
+        }
+        // CraftBukkit end
     }
 
     @Decorate(method = "explode", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;hurt(Lnet/minecraft/world/damagesource/DamageSource;F)Z"))
@@ -81,20 +95,45 @@ public abstract class MixinExplosion implements InjectionExplosion {
         return (boolean) DecorationOps.callsite().invoke(entity, damageSource, f);
     }
 
-    @Decorate(method = "explode", at = @At(value = "NEW", ordinal = 0, target = "(DDD)Lnet/minecraft/world/phys/Vec3;"),
-            slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/ExplosionDamageCalculator;getKnockbackMultiplier(Lnet/minecraft/world/entity/Entity;)F")))
-    private Vec3 banner$knockBack(double d, double e, double f, @Local(ordinal = -1) Entity entity) throws Throwable {
-        var vec3 = (Vec3) DecorationOps.callsite().invoke(d, e, f);
-        double dx = entity.getX() - this.x;
-        double dy = entity.getEyeY() - this.y;
-        double dz = entity.getZ() - this.z;
-        var force = dx * dx + dy * dy + dz * dz;
-        if (entity instanceof LivingEntity) {
-            var result = entity.getDeltaMovement().add(vec3);
-            var event = CraftEventFactory.callEntityKnockbackEvent((CraftLivingEntity) entity.getBukkitEntity(), source, EntityKnockbackEvent.KnockbackCause.EXPLOSION, force, vec3, result.x, result.y, result.z);
-            vec3 = (event.isCancelled()) ? Vec3.ZERO : new Vec3(event.getFinalKnockback().getX(), event.getFinalKnockback().getY(), event.getFinalKnockback().getZ()).subtract(entity.getDeltaMovement());
+    @Redirect(
+            method = "explode",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/entity/Entity;setDeltaMovement(Lnet/minecraft/world/phys/Vec3;)V"
+            )
+    )
+    private void banner$setDeltaMovement(Entity instance, Vec3 vec3) {
+        float q = this.radius * 2.0F;
+        double v = Math.sqrt(instance.distanceToSqr(vec3)) / (double)q;
+        double aa = ((double)1.0F - v) * (double)Explosion.getSeenPercent(vec3, instance) * (double)this.damageCalculator.getKnockbackMultiplier(instance);
+        double ab;
+        if (instance instanceof LivingEntity livingEntity) {
+            ab = aa * ((double)1.0F - livingEntity.getAttributeValue(Attributes.EXPLOSION_KNOCKBACK_RESISTANCE));
+        } else {
+            ab = aa;
         }
-        return vec3;
+        double dx = instance.getX() - this.x;
+        double dy = instance.getEyeY() - this.y;
+        double dz = instance.getZ() - this.z;
+        dx *= ab;
+        dy *= ab;
+        dz *= ab;
+        Vec3 vec31 = new Vec3(dx, dy, dz);
+        Vec3 result = instance.getDeltaMovement().add(vec31);
+        if (instance instanceof LivingEntity livingEntity) {
+            EntityKnockbackEvent event = CraftEventFactory.callEntityKnockbackEvent(
+                    (CraftLivingEntity) livingEntity.getBukkitEntity(),
+                    ((Explosion) (Object) this).getDirectSourceEntity(),
+                    EntityKnockbackEvent.KnockbackCause.EXPLOSION,
+                    ab,
+                    vec31,
+                    result.x,
+                    result.y,
+                    result.z
+            );
+            vec31 = (event.isCancelled()) ? Vec3.ZERO : new Vec3(event.getFinalKnockback().getX(), event.getFinalKnockback().getY(), event.getFinalKnockback().getZ());
+        }
+        instance.setDeltaMovement(vec31);
     }
 
     @Decorate(method = "finalizeExplosion", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;onExplosionHit(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/Explosion;Ljava/util/function/BiConsumer;)V"))
