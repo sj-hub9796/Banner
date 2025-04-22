44c31937f3138824ea201e73779c36a90deef6f0
wdog5 <l1531245281@yandex.com>
2025-04-22 19:46:16 +0800

    Revert "more init"
    This reverts commit 39c8fcbefb215449b69450e366dd49b65427712c.

---
+++ installer/src/main/java/com/mohistmc/banner/installer/InstallInfo.java
@@ -0,0 +1,28 @@
+package com.mohistmc.banner.installer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class InstallInfo {
+
+    public Installer installer;
+    public Map<String, String> libraries;
+    public Map<String, String> fabricExtra;
+
+    public static class Installer {
+
+        public String minecraft;
+        public String forge;
+        public String forgeHash;
+        public String neoforge;
+        public String neoforgeHash;
+        public String fabricLoader;
+        public String fabricLoaderHash;
+    }
+
+    public Map<String, String> fabricDeps() {
+        var map = new HashMap<>(this.libraries);
+        map.putAll(this.fabricExtra);
+        return map;
+    }
+}
