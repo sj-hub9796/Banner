a59f484ce5e45d4926b1ecf794813ab6101446e8
sjhub <sj.hub9796@gmail.com>
2025-07-19 04:19:03 +0900

    migrate overwrite to redirect for mixin compat

--- modules/banner-server/src/main/java/com/mohistmc/banner/mixin/world/level/block/MixinChorusFlowerBlock.java
+++ modules/banner-server/src/main/java/com/mohistmc/banner/mixin/world/level/block/MixinChorusFlowerBlock.java
@@ -1,10 +1,11 @@
 package com.mohistmc.banner.mixin.world.level.block;
 
 import javax.annotation.Nullable;
+
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelReader;
@@ -18,10 +19,10 @@ import net.minecraft.world.phys.BlockHitResult;
 import org.bukkit.craftbukkit.v1_20_R1.event.CraftEventFactory;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ChorusFlowerBlock.class)
@@ -41,81 +42,39 @@ public abstract class MixinChorusFlowerBlock extends Block {
     @Shadow protected abstract void placeDeadFlower(Level worldIn, BlockPos pos);
     // @formatter:on
 
-    /**
-     * @author wdog5
-     * @reason
-     */
-    @Overwrite
-    public void randomTick(BlockState state, ServerLevel worldIn, BlockPos pos, RandomSource random) {
-        BlockPos blockpos = pos.above();
-        if (worldIn.isEmptyBlock(blockpos) && blockpos.getY() < 256) {
-            int i = state.getValue(AGE);
-            if (i < 5) {
-                boolean flag = false;
-                boolean flag1 = false;
-                BlockState blockstate = worldIn.getBlockState(pos.below());
-                Block block = blockstate.getBlock();
-                if (block == Blocks.END_STONE) {
-                    flag = true;
-                } else if (block == this.plant) {
-                    int j = 1;
+    @Redirect(method = "randomTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerLevel;setBlock(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;I)Z", ordinal = 0))
+    private boolean banner$callBlockSpreadEvent0(ServerLevel level, BlockPos pos, BlockState state, int flag, @Local(name = "blockPos") BlockPos blockPos, @Local(name = "i") int i) {
+        if (CraftEventFactory.handleBlockSpreadEvent(level, pos, blockPos, this.defaultBlockState().setValue(ChorusFlowerBlock.AGE, i), 2)) {
+            level.setBlock(pos, this.plant.getStateForPlacement(level, pos), 2);
+            this.placeGrownFlower(level, blockPos, i);
+            return true;
+        }
+        return false;
+    }
 
-                    for (int k = 0; k < 4; ++k) {
-                        Block block1 = worldIn.getBlockState(pos.below(j + 1)).getBlock();
-                        if (block1 != this.plant) {
-                            if (block1 == Blocks.END_STONE) {
-                                flag1 = true;
-                            }
-                            break;
-                        }
+    @Redirect(method = "randomTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/ChorusFlowerBlock;placeGrownFlower(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;I)V", ordinal = 0))
+    private void banner$cancelPlaceGrownFlower(ChorusFlowerBlock instance, Level level, BlockPos pos, int age) {
+        // do nothing
+    }
 
-                        ++j;
-                    }
+    @Redirect(method = "randomTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/ChorusFlowerBlock;placeGrownFlower(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;I)V", ordinal = 1))
+    private void banner$callBlockSpreadEvent1(ChorusFlowerBlock instance, Level level, BlockPos pos, int age, @Local(name = "blockPos2") BlockPos blockPos2, @Local(name = "i") int i) {
+        if (CraftEventFactory.handleBlockSpreadEvent(level, pos, blockPos2, this.defaultBlockState().setValue(ChorusFlowerBlock.AGE, i + 1), 2)) {
+            this.placeGrownFlower(level, blockPos2, i + 1);
+        }
+    }
 
-                    if (j < 2 || j <= random.nextInt(flag1 ? 5 : 4)) {
-                        flag = true;
-                    }
-                } else if (blockstate.isAir()) {
-                    flag = true;
-                }
+    @Redirect(method = "randomTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/ChorusFlowerBlock;placeDeadFlower(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;)V", ordinal = 0))
+    private void banner$callBlockGrowEvent0(ChorusFlowerBlock instance, Level level, BlockPos pos) {
+        if (CraftEventFactory.handleBlockGrowEvent(level, pos, this.defaultBlockState().setValue(ChorusFlowerBlock.AGE, 5), 2)) {
+            this.placeDeadFlower(level, pos);
+        }
+    }
 
-                if (flag && allNeighborsEmpty(worldIn, blockpos, (Direction) null) && worldIn.isEmptyBlock(pos.above(2))) {
-                    if (CraftEventFactory.handleBlockSpreadEvent(worldIn, pos, blockpos, this.defaultBlockState().setValue(ChorusFlowerBlock.AGE, i), 2)) {
-                        worldIn.setBlock(pos, this.plant.getStateForPlacement(worldIn, pos), 2);
-                        this.placeGrownFlower(worldIn, blockpos, i);
-                    }
-                } else if (i < 4) {
-                    int l = random.nextInt(4);
-                    if (flag1) {
-                        ++l;
-                    }
-
-                    boolean flag2 = false;
-
-                    for (int i1 = 0; i1 < l; ++i1) {
-                        Direction direction = Direction.Plane.HORIZONTAL.getRandomDirection(random);
-                        BlockPos blockpos1 = pos.relative(direction);
-                        if (worldIn.isEmptyBlock(blockpos1) && worldIn.isEmptyBlock(blockpos1.below()) && allNeighborsEmpty(worldIn, blockpos1, direction.getOpposite())) {
-                            if (CraftEventFactory.handleBlockSpreadEvent(worldIn, pos, blockpos1, this.defaultBlockState().setValue(ChorusFlowerBlock.AGE, i + 1), 2)) {
-                                this.placeGrownFlower(worldIn, blockpos1, i + 1);
-                                flag2 = true;
-                            }
-                        }
-                    }
-
-                    if (flag2) {
-                        worldIn.setBlock(pos, this.plant.getStateForPlacement(worldIn, pos), 2);
-                    } else {
-                        if (CraftEventFactory.handleBlockGrowEvent(worldIn, pos, this.defaultBlockState().setValue(ChorusFlowerBlock.AGE, 5), 2)) {
-                            this.placeDeadFlower(worldIn, pos);
-                        }
-                    }
-                } else {
-                    if (CraftEventFactory.handleBlockGrowEvent(worldIn, pos, this.defaultBlockState().setValue(ChorusFlowerBlock.AGE, 5), 2)) {
-                        this.placeDeadFlower(worldIn, pos);
-                    }
-                }
-            }
+    @Redirect(method = "randomTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/ChorusFlowerBlock;placeDeadFlower(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;)V", ordinal = 1))
+    private void banner$callBlockGrowEvent1(ChorusFlowerBlock instance, Level level, BlockPos pos) {
+        if (CraftEventFactory.handleBlockGrowEvent(level, pos, this.defaultBlockState().setValue(ChorusFlowerBlock.AGE, 5), 2)) {
+            this.placeDeadFlower(level, pos);
         }
     }
 
